b73cc2787f4060a7b5c52f16ca43982014a5e67d
Revengeslayer <gccbs05591gw2@gmail.com>
2023-04-29 17:18:44 +0800

    RestfulApi v1
    create a new project , create a "MyApi" and create some endpoint

---
+++ RestfulApi/RestfulApi/Controllers/MyApiController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Encodings.Web;
+
+namespace RestfulApi.Controllers
+{
+    [ApiController]
+    //url must have api/
+    //[Route("{prefix:regex(api)}/[controller]")]
+
+    //url add myapi/
+    //[Route("myapi/[controller]")]
+    [Route("[controller]")]
+    public class MyApiController : ControllerBase
+    {
+
+        //HttpGet() url will define this first   
+        [HttpGet]      
+        //url = MyApi/Show       
+        [HttpGet("Show")]
+        public IActionResult Show()
+        {
+  
+            var result = new { Message = "url=~/MyApi/Show" };
+            return Ok(result);
+        }
+        // this {id?} is dispensable
+        [HttpGet("Display/{id?}")]
+        public IActionResult Display(string displayer,int? id) 
+        {
+
+            var result = new { Message = "Hello, " + displayer +"     "+id};
+            return Ok(result);
+
+        }
+
+        
+    }
+}
